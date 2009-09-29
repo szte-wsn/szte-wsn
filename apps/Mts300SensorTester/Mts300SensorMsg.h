6f49b6ea8bf803f266cce27f71375149339f6766
Zoltan Kincses <kincseszoltan@hotmail.com>
2009-09-29 15:14:14 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@6 a4e16f3a-606c-4714-833f-ed604d7aca08

---
+++ apps/Mts300SensorTester/Mts300SensorMsg.h
@@ -0,0 +1,25 @@
+#ifndef SENSORMSG_H
+#define SENSORMSG_H
+
+enum{
+	AM_CONTROLMSG = 1,
+	AM_DATAMSG = 2,
+};
+
+typedef nx_struct controlmsg{
+	nx_uint8_t nodeID;
+	nx_uint8_t instr;
+}controlmsg_t;
+
+typedef nx_struct datamsg{
+	nx_uint8_t nodeID;
+	nx_uint16_t min;
+	nx_uint16_t max;
+	nx_uint16_t sampleCnt;
+	nx_uint16_t micSampPer;
+	nx_uint16_t micSampNum;
+	nx_uint32_t average;
+	nx_uint32_t energy;
+}datamsg_t;
+
+#endif
