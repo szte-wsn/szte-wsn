54418beb410b4a902e6050504ba359c983db1f4c
Krisztian Veress <veresskrisztian@gmail.com>
2009-10-16 08:27:19 +0000

    nodenum added for more flexible debugging
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@23 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/python/RadioTest/testrun.py
+++ support/python/RadioTest/testrun.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 
 from TOSSIM import *;
-#from test_message_t import *;
-#from setup_t import *;
-
 import sys;
+
+nodenum = 2;
 
 t = Tossim([]);
 t.addChannel("Debug",sys.stdout);
-t.addChannel("Pending",sys.stdout);
-t.addChannel("Radio",sys.stdout);
 
 # Setup Radio topology and noise
-"""r = t.radio();
+r = t.radio();
 f = open("topo.txt", "r")
 lines = f.readlines()
 for line in lines:
@@ -27,42 +24,16 @@ for line in lines:
   str = line.strip()
   if (str != ""):
     val = int(str)
-    for i in range(1, 6):
+    for i in range(1, nodenum+1):
       t.getNode(i).addNoiseTraceReading(val)
 
-for i in range(1, 6):
+for i in range(1, nodenum+1):
   print "Creating noise model for ",i;
   t.getNode(i).createNoiseModel()
-"""
+
 # Boot motes
-t.getNode(1).bootAtTime(21213);
-t.getNode(5).bootAtTime(21213);
-t.getNode(3).bootAtTime(21213);
-t.getNode(2).bootAtTime(21213);
-t.getNode(4).bootAtTime(21213);
-
-# Construct an injectable packet
-"""msg = test_message_t();
-msg.set_type(1);
-
-setup = setup_t();
-setup.set_nodenum(10);
-setup.set_msgsize(30);
-setup.set_radiopolicy(4+32);
-
-setup.set_lpl_localSleepInterval(0);
-setup.set_lpl_localDutyCycle(0);
-setup.set_lpl_rxSleepInterval(0);
-setup.set_lpl_rxDutyCycle(0);
-setup.set_lpl_dutyCycleToSleep(0);
-
-msg.set_data(setup);
-# packet ready
-
-pkt = t.newPacket();
-pkt.setData(msg.data)
-pkt.setType(msg.get_amType())
-pkt.setDestination(2)"""
+for i in range(1, nodenum+1):
+  t.getNode(i).bootAtTime(21213);
 
 for i in range(0, 10000):
   t.runNextEvent()
