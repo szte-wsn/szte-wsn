b6dd925899d761f0bd623f7a302aad6c639a3cba
Krisztian Veress <veresskrisztian@gmail.com>
2009-11-25 10:57:33 +0000

    Control message injection added in TOSSIM simulation
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@44 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/python/RadioTest/testrun.py
+++ support/python/RadioTest/testrun.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
-from TOSSIM import *;
-import sys;
+from TOSSIM import *
+from setup_t import *
+import sys
 
-nodenum = 2;
+nodenum = 3
 
-t = Tossim([]);
-t.addChannel("Debug",sys.stdout);
+t = Tossim([])
+r = t.radio();
+
+t.addChannel("Debug",sys.stdout)
+
+# Boot motes
+for i in range(0, nodenum):
+  t.getNode(i).bootAtTime((31 + t.ticksPerSecond() / 10) * i + 1);
 
 # Setup Radio topology and noise
-r = t.radio();
 f = open("topo.txt", "r")
 lines = f.readlines()
 for line in lines:
@@ -24,16 +30,32 @@ for line in lines:
   str = line.strip()
   if (str != ""):
     val = int(str)
-    for i in range(1, nodenum+1):
+    for i in range(0, nodenum):
       t.getNode(i).addNoiseTraceReading(val)
 
-for i in range(1, nodenum+1):
+for i in range(0, nodenum):
   print "Creating noise model for ",i;
   t.getNode(i).createNoiseModel()
 
-# Boot motes
-for i in range(1, nodenum+1):
-  t.getNode(i).bootAtTime(21213);
+for i in range(0, 10):
+  t.runNextEvent();
 
-for i in range(0, 10000):
+# Inject control message
+msg = setup_t()
+msg.set_problem_idx(0)
+msg.set_runtime_msec(1000)
+msg.set_sendtrig_msec(100)
+msg.set_flags(0)
+
+pkt = t.newPacket()
+pkt.setData(msg.data)
+pkt.setType(msg.get_amType())
+pkt.setDestination(0xffff);
+pkt.setStrength(1000);
+
+print "Injecting CTRL packets ";
+for i in range(0, nodenum):
+  pkt.deliver(i, t.time() + 3)
+
+for i in range(0, 1000):
   t.runNextEvent()
