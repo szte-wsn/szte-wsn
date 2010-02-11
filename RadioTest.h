44ce0c1096643ab61d04540a8175af87ac961868
Krisztian Veress <veresskrisztian@gmail.com>
2010-02-11 13:46:45 +0000

    sendBusyCount, notAckedCount intro, responsemsg_t, TOSSIM support removed, STATE_LASTCHANCE added
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk/apps/RadioTest@137 a4e16f3a-606c-4714-833f-ed604d7aca08

--- RadioTest.h
+++ RadioTest.h
@@ -36,20 +36,16 @@
 #ifndef RADIO_TEST_H
 #define RADIO_TEST_H
 
-#define MAX_EDGE_COUNT 8
+#define MAX_EDGE_COUNT 6
 #define MAX_NODE_COUNT 3
 
 #if MAX_EDGE_COUNT <= 8
-  typedef nx_uint8_t nx_pending_t;
   typedef uint8_t pending_t;
 #elif MAX_EDGE_COUNT <= 16
-  typedef nx_uint16_t nx_pending_t;
   typedef uint16_t pending_t;
 #elif MAX_EDGE_COUNT <= 32
-  typedef nx_uint32_t nx_pending_t;
   typedef uint32_t pending_t;
 #elif MAX_EDGE_COUNT <= 64
-  typedef nx_uint64_t nx_pending_t;
   typedef uint64_t pending_t;
 #else
   #error "MAX_EDGE_COUNT is set too high! The current limit is 64!"
@@ -57,116 +53,110 @@
 
 enum {
   // Mote states
-  STATE_INVALID = 0x0,
-  STATE_IDLE = 0x1,
-  STATE_SETUP_RCVD = 0x2,
-  STATE_CONFIGURED = 0x3,
-  STATE_RUNNING = 0x4,
-  STATE_FINISHED = 0x5,
-  STATE_UPLOADING = 0x7,
+  STATE_INVALID     = 0x0,
+  STATE_IDLE        = 0x1,
+  STATE_SETUP_RCVD  = 0x2,
+  STATE_CONFIGURED  = 0x3,
+  STATE_RUNNING     = 0x4,
+  STATE_LASTCHANCE  = 0x5,
+  STATE_FINISHED    = 0x6,
+  STATE_UPLOADING   = 0x7,
 
   // Policy flags
-  USE_ACK = 0x01,
-  USE_LPL = 0x02,
-  USE_DIRECT_ADDR = 0x03,
+  USE_ACK           = 0x1,
+  USE_DADDR         = 0x2,
+  USE_LPL           = 0x4,
 
   // AM Type identifiers
-  AM_CTRLMSG_T = 101,
-  AM_TESTMSG_T = 102,
+  AM_CTRLMSG_T      = 101,
+  AM_TESTMSG_T      = 102,
+  AM_RESPONSEMSG_T  = 103,
 
-  // Control message types
-  CTRL_SETUP = 0,
-  CTRL_SETUP_SYN = 1,
-  CTRL_SETUP_ACK = 2,
+  // Control / Response message types
+  CTRL_SETUP        = 0,
+  CTRL_SETUP_SYN    = 1,
+  RESP_SETUP_ACK    = 2,
 
-  CTRL_START = 10,
-  CTRL_RESET = 20,
+  CTRL_START        = 10,
+  CTRL_RESET        = 20,
 
-  CTRL_STAT_REQ = 30,
-  CTRL_STAT_OK = 31,
-  CTRL_STAT_NEXISTS = 32
+  CTRL_DATA_REQ     = 30,
+  RESP_DATA_OK      = 31,
+
+  CTRL_DBG_REQ      = 40,
+  RESP_DBG_OK       = 41,
+
+  RESP_DATA_NEXISTS  = 50
 };
 
 // Edge type
 typedef struct edge_t {
-  uint8_t    sender;       // Sender end of the edge
-  uint8_t    receiver;     // Receiver end of the edge
-  uint8_t    flags;        // Sending policies
-  uint32_t   nextmsgid;    // The message id to send ( on send side ) / expected to receive ( on receive side )
-  uint32_t   lastmsgid;    // The last message id sent ( on send side ). On receive side it is unused.
-  pending_t  pongs;        // In ping-pong policy the edge bitmask on which we should reply on receive
+  uint8_t       sender;           // Sender end of the edge
+  uint8_t       receiver;         // Receiver end of the edge
+  uint8_t       flags;            // Sending policies
+  uint16_t      nextmsgid;        // The message id to send (on send side)/expected to receive (on receive side)
+  uint16_t      lastmsgid;        // The last message id sent (on send side). On receive side it is unused.
+  pending_t     pongs;            // In ping-pong policy the edge bitmask on which we should reply on receive
 } edge_t;
 
 
 // Setup type
 typedef nx_struct setup_t {
-  nx_uint8_t  problem_idx;    // The problem we should test
-  nx_uint32_t runtime_msec;   // How long should we run the test?
-  nx_uint32_t sendtrig_msec;  // How often we should send a message when sending on timer ticks ?
-  nx_uint8_t  flags;          // Global flags ( such as ACK, LPL, ... )
+  nx_uint8_t    problem_idx;      // The problem we should test
+  nx_uint32_t   runtime_msec;     // How long should we run the test?
+  nx_uint32_t   sendtrig_msec;    // How often we should send a message when sending on timer ticks ?
+  nx_uint32_t   lastchance_msec;  // How long should we wait for reception after the running time has elapsed ?
+  nx_uint8_t    flags;            // Global flags ( such as ACK, LPL, ... )
+  nx_uint16_t   lplwakeupintval;  // Low Power Listening wakeup interval
 } setup_t;
 
-// Stats base type
+
+// Base type
 typedef nx_uint16_t stat_base_t;
+
 // Stats type
 typedef nx_struct stat_t {
-  stat_base_t sendSuccessCount;
-  stat_base_t sendFailCount;
-  stat_base_t sendDoneSuccessCount;
-  stat_base_t sendDoneFailCount;
+  stat_base_t   sendSuccessCount;
+  stat_base_t   sendFailCount;
+  stat_base_t   sendBusyCount;
 
-  stat_base_t wasAckedCount;
-  stat_base_t resendCount;
+  stat_base_t   sendDoneSuccessCount;
+  stat_base_t   sendDoneFailCount;
 
-  stat_base_t receiveCount;
-  stat_base_t duplicateReceiveCount;
-  stat_base_t missedCount;
+  stat_base_t   wasAckedCount;
+  stat_base_t   notAckedCount;
+  stat_base_t   resendCount;
+  stat_base_t   wouldBacklogCount;
 
-  stat_base_t wouldBacklogCount;
+  stat_base_t   receiveCount;
+  stat_base_t   duplicateCount;
+  stat_base_t   missedCount;
 } stat_t;
 
 
 typedef nx_struct testmsg_t {
-  nx_uint8_t  edgeid;       // On which edge this message is intended to propagate through
-  nx_uint32_t msgid;        // The auto-increment id of the message on the preset edge
+  nx_uint8_t    edgeid;           // On which edge this message is intended to propagate through
+  nx_uint16_t   msgid;            // The auto-increment id of the message on the preset edge
 } testmsg_t;
 
 typedef nx_struct ctrlmsg_t {
-  nx_uint8_t    type;         // Control type
-  nx_union {
-    setup_t     config;       // The config in the setup stage
-    nx_struct {
-      nx_uint8_t  statidx;      // The requested stat index in the requesting stage
-      stat_t      statpayload;  // The requested stat in the downloading stage
-    } stat;
-  } data;
+  nx_uint8_t    type;             // Control type
+  nx_uint8_t    reqidx;           // The requested stat-edge pair index in the requesting stage
+  setup_t       config;           // The config in the setup stage
 } ctrlmsg_t;
 
-void dbgbin(pending_t data)
-{
-  pending_t bit = 1 << (sizeof(pending_t)*8-1);
-  char c[sizeof(pending_t)*8+1];
-  uint8_t i = 0;
-  while( bit ) {
-    c[i++] = ( data & bit ) ? '1' : '0';
-    bit >>= 1;
-  }
-  c[sizeof(pending_t)*8] = '\0';
-  dbg("Debug","%s\n",c);
-}
-
-void dbgstat(stat_t s) {
-  dbg("Debug","stat.sendSuccessCount     : %d\n",s.sendSuccessCount);
-  dbg("Debug","stat.sendFailCount        : %d\n",s.sendFailCount);
-  dbg("Debug","stat.sendDoneSuccessCount : %d\n",s.sendDoneSuccessCount);
-  dbg("Debug","stat.sendDoneFailCount    : %d\n",s.sendDoneFailCount);
-  dbg("Debug","stat.wasAckedCount        : %d\n",s.wasAckedCount);
-  dbg("Debug","stat.resendCount          : %d\n",s.resendCount);
-  dbg("Debug","stat.receiveCount         : %d\n",s.receiveCount);
-  dbg("Debug","stat.duplicateReceiveCount: %d\n",s.duplicateReceiveCount);
-  dbg("Debug","stat.missedCount          : %d\n",s.missedCount);
-  dbg("Debug","stat.wouldBacklogCount    : %d\n",s.wouldBacklogCount);
-  dbg("Debug","---------------------------------------------\n");
-}
+typedef nx_struct responsemsg_t {
+  nx_uint8_t    type;             // Response type
+  nx_uint8_t    respidx;          // The requested stat-edge pair index in the requesting stage
+  nx_union {
+    stat_t      stat;             // The requested stat structure in the requesting stage
+    nx_struct {
+      nx_uint8_t    endtype;      //  1 : Sender / 2 : Receiver / 3: Nothing
+      nx_uint16_t   nextmsgid;    // The final value of nextmsgid on the requested edge.
+      nx_uint16_t   lastmsgid;    // The final value of lastmsgid on the requested edge.
+      nx_uint32_t   dbgLINE;      // Assertion LINE value if any assertion failed.
+    }           debug;
+  } payload;
+} responsemsg_t;
 
 #endif
