25e662f7c99bac60b1755ba4baeb5686da3c3e15
Ali Baharev <ali.baharev@gmail.com>
2010-06-04 15:13:17 +0000

    still testing
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@246 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/cpp/ali/src/main.cpp
+++ support/cpp/ali/src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <cassert>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	string m("Hello World!");
+	cout <<  m << endl;
+	vector<int> v;
+    v.push_back(2);
+    v.push_back(1);
+    assert(false);
 	return 0;
 }
