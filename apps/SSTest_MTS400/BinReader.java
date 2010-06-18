9e555f59952325e12e763f7e80d35c5b6d094543
Andras Biro <bbandi86@gmail.com>
2010-06-18 13:59:42 +0000

    timesync now calculated with linear regression, capable to handle restarted motes
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@263 a4e16f3a-606c-4714-833f-ed604d7aca08

--- apps/SSTest_MTS400/BinReader.java
+++ apps/SSTest_MTS400/BinReader.java
@@ -50,6 +50,7 @@ public class BinReader {
 	private final byte XORESCAPE=0x20;
 	private dataFile datas;
 	private HashSet<Long> gaps;
+	private ArrayList<TimeFunctions> functions=new ArrayList<TimeFunctions>(); 
 	
 	private ArrayList<Integer> readNextFrame(RandomAccessFile file_input) throws IOException{
 		if(file_input.getFilePointer()>=file_input.length())
@@ -80,7 +81,41 @@ public class BinReader {
 		return onemeas;
 	}
 	
-	public BinReader(String datafile, String outputfile, boolean convert, boolean converttime, boolean rewrite){
+	private TimeFunctions regression(ArrayList<Long> MoteTime,ArrayList<Long> PCTime){
+		double skew,offset;
+		if(MoteTime.size()!=PCTime.size()||MoteTime.size()<0)
+			return null;
+		if(MoteTime.size()>=2){//linear regression
+			double av_mote=0, av_pc=0;
+			for(int i=0;i<MoteTime.size();i++){
+				av_mote+=MoteTime.get(i);
+				av_pc+=PCTime.get(i);
+			}
+			av_mote/=MoteTime.size();
+			av_pc/=PCTime.size();
+			double denom=0,numer=0;
+			for(int i=0;i<MoteTime.size();i++){
+				numer+=(MoteTime.get(i)-av_mote)*(PCTime.get(i)-av_pc);
+				denom+=(MoteTime.get(i)-av_mote)*(MoteTime.get(i)-av_mote);
+			}
+			skew=numer/denom;
+			offset=av_pc-skew*av_mote;							
+		} else{
+			skew=1;
+			offset=PCTime.get(0)-MoteTime.get(0);
+		}
+		return new TimeFunctions(offset, skew);
+	}
+	
+	private double checkerror(TimeFunctions func, long pc, long mote){
+		double ret=pc-func.offset-func.skew*mote;
+		if(ret<0)
+			return -1*ret;
+		else	
+			return ret;
+	}
+	
+	public BinReader(String datafile, String outputfile, boolean convert, boolean converttime, boolean rewrite, long errorlimit){
 		if(!new File(datafile).exists()){
 			System.err.println("Data file doesn't exist");
 			System.exit(1);
@@ -91,21 +126,30 @@ public class BinReader {
 		} 
 		try {
 			datas=new dataFile(datafile);
-			long timeoffset=0;
+			
 			if(datas.getTimestamps().exists()){
 				BufferedReader input =  new BufferedReader(new FileReader(datas.getTimestamps()));
-				long timestamps=0;
 				String line;
+				TimeFunctions tempfunc=null;
+				ArrayList<Long> MoteTime=new ArrayList<Long>();
+				ArrayList<Long> PCTime=new ArrayList<Long>();
 				while (( line = input.readLine()) != null){
 					String[] dates = line.split(":");
-					long locale=Long.parseLong(dates[0]);
-					long remote=Long.parseLong(dates[1]);
-					timeoffset+=(locale-remote);
-					timestamps++;
+					if(tempfunc!=null&&checkerror(tempfunc,Long.parseLong(dates[0]),Long.parseLong(dates[1]))>errorlimit){//end of running: calculate the function, then read the next running
+						functions.add(tempfunc);
+						System.out.println("pc="+tempfunc.offset+"+"+tempfunc.skew+"*mote; points:"+MoteTime.size());
+						PCTime.clear();
+						MoteTime.clear();
+						tempfunc=null;
+					}
+					PCTime.add(Long.parseLong(dates[0]));
+					MoteTime.add(Long.parseLong(dates[1]));
+					tempfunc=regression(MoteTime, PCTime);
 				}
-				timeoffset/=timestamps;
+				functions.add(tempfunc);
+				System.out.println("pc="+tempfunc.offset+"+"+tempfunc.skew+"*mote; points:"+MoteTime.size());
 			} else {
-				System.out.println("Warning: Timestamp file doesn't exist, timeoffset=0");
+				System.out.println("Warning: Timestamp file doesn't exist");
 			}
 			
 			gaps=datas.getAllGap();
@@ -127,112 +171,72 @@ public class BinReader {
 					} else
 						de.light=0xffff;
 					
-					de.time=frame.get(5)*16777216L+frame.get(6)*65536L+frame.get(7)*256L+frame.get(8);
-					de.time+=timeoffset;
+					de.localetime=frame.get(5)*16777216L+frame.get(6)*65536L+frame.get(7)*256L+frame.get(8);
 					data.add(de);
 					//System.out.println(frame);
 				} else 
 					System.out.println("Bad frame");
 			}
+			int currentfunc=functions.size()-1;
+			long prevtime=Long.MAX_VALUE;
+			for(int i=data.size()-1;i>=0;i--){
+				if(prevtime<=data.get(i).localetime)
+					currentfunc--;
+				if(currentfunc<0){
+					data.get(i).globaltime=data.get(i).localetime;
+				}else{
+					prevtime=data.get(i).localetime;
+					data.get(i).globaltime=(long) (functions.get(currentfunc).offset+functions.get(currentfunc).skew*data.get(i).localetime);
+				}
+			}
 			BufferedWriter output = new BufferedWriter(new FileWriter(outputfile));
-			output.write("Time,Temperature,Humidity,Brightness");
+			output.write("LocalTime,GlobalTime,Temperature,Humidity,Brightness");
 			output.newLine();
 			for(DataElement de:data){
 				String temp=Integer.toString(de.temp);
 				String humi=Integer.toString(de.humi);
-				String time=Long.toString(de.time);
+				String globaltime=Long.toString(de.globaltime);
+				String localetime=Long.toString(de.localetime);
 				if(converttime){
-					time=DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(new Date(de.time));
+					globaltime=DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(new Date(de.globaltime));
 					//time=new Date(de.time).toString();
 				}	
 				if(convert){
 					temp=Double.toString(0.01*de.temp-39.6);
 					humi=Double.toString(-4+0.0405*de.humi+-2.8000E-6*de.humi*de.humi);
 				}
-				System.out.append(time +","+temp+","+humi+","+de.light+"\n");
-				output.append(time +","+temp+","+humi+","+de.light);
+				System.out.append(localetime + "," + globaltime +","+temp+","+humi+","+de.light+"\n");
+				output.append(localetime + "," + globaltime +","+temp+","+humi+","+de.light);
 				output.newLine();
 			}
 			output.flush();
 			output.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Error: Can't write output file");
 		}
 	}
 	
-//	public BinReader(int nodeid,boolean physical){
-//		try {
-//			datas=new dataFile(nodeid);
-//			long timeoffset=0;
-//			if(datas.getTimestamps().exists()){
-//				BufferedReader input =  new BufferedReader(new FileReader(datas.getTimestamps()));
-//				long timestamps=0;
-//				String line;
-//				while (( line = input.readLine()) != null){
-//					String[] dates = line.split(":");
-//					long locale=Long.parseLong(dates[0]);
-//					long remote=Long.parseLong(dates[1]);
-//					timeoffset+=(locale-remote);
-//					timestamps++;
-//				}
-//				timeoffset/=timestamps;
-//			} else {
-//				System.out.println("Warning: Timestamp file doesn't exist, timeoffset=0");
-//			}
-//			
-//			gaps=datas.getAllGap();
-//			RandomAccessFile file_input = datas.dataFile;
-//			
-//			while(file_input.getFilePointer()<file_input.length()){
-//				//System.out.println(file_input.getFilePointer()+"|"+file_input.length());
-//				ArrayList<Integer> frame=readNextFrame(file_input);
-//				if(frame.size()==9){
-//					DataElement de=new DataElement();
-//					de.temp=frame.get(0)*256+frame.get(1);
-//					de.humi=frame.get(2)*256+frame.get(3);
-//					de.light=frame.get(4);
-//					if(de.light>127&&de.light<256){
-//						int s=0xf&de.light;
-//						int c=Integer.rotateRight(de.light-128-s,4);
-//						int twopowc=(int) Math.pow(2,c);
-//						de.light=(int) (Math.floor(16.5*(twopowc-1)))+s*twopowc;
-//					} else
-//						de.light=0xffff;
-//					
-//					de.time=frame.get(5)*16777216L+frame.get(6)*65536L+frame.get(7)*256L+frame.get(8);
-//					de.time+=timeoffset;
-//					data.add(de);
-//					//System.out.println(frame);
-//				} else 
-//					System.out.println("Bad frame");
-//			}
-//			for(DataElement de:data){
-//				if(physical){
-//					double temp=0.01*de.temp-39.6;
-//					double humi=-4+0.0405*de.humi+-2.8000E-6*de.humi*de.humi;
-//					System.out.println(de.time +":"+temp+":"+humi+":"+de.light);
-//				} else
-//					System.out.println(de.time +":"+de.temp+":"+de.humi+":"+de.light);
-//			}
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//				
-//		
-//	}
-	
 	public class DataElement{
 		int temp, humi,light;
-		long time;
+		long localetime;
+		long globaltime;
+	}
+	
+	public class TimeFunctions{
+		double skew, offset;
+		
+		public TimeFunctions(double offset, double skew){
+			this.offset=offset;
+			this.skew=skew;
+		}
 	}
 	
 	public static void usageThanExit(){
-		System.out.println("java BinReader [options] <datafile> <outputfile>");
+		System.out.println("java BinReader [options] [maxtimeerror] <datafile> <outputfile>");
+		System.out.println("<datafile>: the binary file to process");
+		System.out.println("<outputfile>: the generated csv file");
+		System.out.println("[maxtimeerror]: The maximum of the enabled error during timesync in seconds");
+		System.out.println("				default:120");
 		System.out.println("options:");
 		System.out.println("	--convert: convert the temperature to celsius and the humidity to percent");
 		System.out.println("	--converttime: convert the time to human readable format");
@@ -246,6 +250,7 @@ public class BinReader {
 		boolean convert=false;
 		boolean converttime=false;
 		boolean rewrite=false;
+		long errorlimit=120000;
 		for(int i=0;i<args.length-2;i++){
 			if(args[i].equals("--convert"))
 				convert=true;
@@ -253,10 +258,15 @@ public class BinReader {
 				converttime=true;
 			else if(args[i].equals("--rewrite"))
 				rewrite=true;
-			else
-				BinReader.usageThanExit();
+			else {
+				try{
+					errorlimit=1000*Long.parseLong(args[i]);
+				} catch(NumberFormatException e){
+					BinReader.usageThanExit();
+				}
+			}
 		}
-		new BinReader(args[args.length-2], args[args.length-1], convert, converttime, rewrite);
+		new BinReader(args[args.length-2], args[args.length-1], convert, converttime, rewrite, errorlimit);
 	}
 
 }
