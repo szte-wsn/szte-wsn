7fcb12d8f400e5b24183c82c8f24affaf659d583
Andras Biro <bbandi86@gmail.com>
2010-07-02 14:22:02 +0000

    licence added, all variable is private now
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@274 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/java/DataProcess/RawPacketConsumer.java
+++ support/java/DataProcess/RawPacketConsumer.java
@@ -1,6 +1,39 @@
-
+/*
+* Copyright (c) 2010, University of Szeged
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* - Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* - Redistributions in binary form must reproduce the above
+* copyright notice, this list of conditions and the following
+* disclaimer in the documentation and/or other materials provided
+* with the distribution.
+* - Neither the name of University of Szeged nor the names of its
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+* OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Author:Andras Biro, Miklos Toth
+*/
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,12 +41,14 @@ import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
 public class RawPacketConsumer{
-	RandomAccessFile dataFile;
+	private File dataFile;
 	private ArrayList<Byte[]>frames=new ArrayList<Byte[]>();
 	private ArrayList<Gap> gaps = new ArrayList<Gap>();
     
-	long maxaddress;
-	int nodeid;	
+	private long maxaddress;
+	private int nodeid;	
+
+	
 	private File gapFile; 	
 
 	public Byte[] readNextFrame(byte[] buffer, int i, byte frame, byte escape, byte xorescaped){
@@ -48,10 +83,9 @@ public class RawPacketConsumer{
 	
 	public ArrayList<Byte[]> makeFrames(byte frame, byte escape, byte xorescaped) throws IOException{
 		byte[] buffer=new byte[(int) dataFile.length()];//TODO: 2GB limit: is it a problem?
-		synchronized (dataFile) {
-			dataFile.seek(0);
-			dataFile.readFully(buffer);
-		}
+		FileInputStream filereader=new FileInputStream(dataFile);
+		filereader.read(buffer);
+		filereader.close();
 		int pointer=0;
 		ArrayList<Byte[]>ret=new ArrayList<Byte[]>();
 		while(pointer<=dataFile.length()){
@@ -78,7 +112,7 @@ public class RawPacketConsumer{
 		
 	}
 	private void initDataFile(String path, String gapPath, int nodeid) throws IOException{
-			this.dataFile=new RandomAccessFile(path,"r");			
+			this.dataFile=new File(path);			
 			this.nodeid=nodeid;
 			System.out.print("Found datafile from #"+nodeid+". opening file:");
 			maxaddress=dataFile.length()-1;
@@ -105,11 +139,7 @@ public class RawPacketConsumer{
 	}
 	
 	private void addGap(long start, long end, boolean unrepairable) {
-		Gap newGap = new Gap();
-		newGap.setStart(start);
-		newGap.setEnd(end);
-		newGap.setUnrepairable(unrepairable);
-		gaps.add(newGap);
+		gaps.add(new Gap(start,end,unrepairable));
 		
 	}	
 	
@@ -124,5 +154,21 @@ public class RawPacketConsumer{
 	}
 	public ArrayList<Byte[]> getFrames() {
 		return frames;
+	}
+	
+	public long getMaxaddress() {
+		return maxaddress;
+	}
+
+	public int getNodeid() {
+		return nodeid;
+	}
+
+	public File getGapFile() {
+		return gapFile;
+	}
+	
+	public File getDataFile() {
+		return dataFile;
 	}
 }
