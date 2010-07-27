e92d754ca61337bfed1f27eee3ab08a290033cb5
Miklos Toth <tothmiklos86@gmail.com>
2010-07-27 15:11:14 +0000

    comments added
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@460 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/java/org/szte/wsn/dataprocess/BinaryInterface.java
+++ support/java/org/szte/wsn/dataprocess/BinaryInterface.java
@@ -29,7 +29,7 @@
 * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 * OF THE POSSIBILITY OF SUCH DAMAGE.
 *
-* Author:Andras Biro, Miklos Toth
+* Author: Miklos Toth
 */
 package org.szte.wsn.dataprocess;
 
