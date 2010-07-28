8adce27edfc67862f3366122348817182ad01d56
Andras Biro <bbandi86@gmail.com>
2010-07-28 13:56:37 +0000

    initial commit
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@487 a4e16f3a-606c-4714-833f-ed604d7aca08

---
+++ support/java/org/szte/wsn/dataprocess/ToSerial.java
@@ -0,0 +1,115 @@
+/*
+* Copyright (c) 2010, University of Szeged
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* - Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* - Redistributions in binary form must reproduce the above
+* copyright notice, this list of conditions and the following
+* disclaimer in the documentation and/or other materials provided
+* with the distribution.
+* - Neither the name of University of Szeged nor the names of its
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+* OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Andras Biro
+*/
+package org.szte.wsn.dataprocess;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import net.tinyos.packet.BuildSource;
+import net.tinyos.packet.PacketListenerIF;
+import net.tinyos.packet.PhoenixSource;
+import net.tinyos.util.PrintStreamMessenger;
+
+public class ToSerial implements BinaryInterface{
+	
+	private PhoenixSource phoenix;
+	ArrayList<byte[]> readbuffer=new ArrayList<byte[]>();
+	
+	public class Listener implements PacketListenerIF{
+		@Override
+		public void packetReceived(byte[] packet) {
+			readbuffer.add(packet);
+		}		
+	}
+	
+	public ToSerial(String source){
+		PhoenixSource phoenix;
+		//TODO: Maybe we should process the error messages
+		if (source == null) {
+			phoenix = BuildSource.makePhoenix(PrintStreamMessenger.err);
+		} else {
+			phoenix = BuildSource.makePhoenix(source, PrintStreamMessenger.err);
+		}
+		phoenix.registerPacketListener(new Listener());
+	}
+	
+
+	public ToSerial(){
+		new ToSerial(null);
+	}
+	
+	@Override
+	public ArrayList<byte[]> readPackets() {
+		while(readbuffer.isEmpty()){
+			try {
+				wait(10);
+			} catch (InterruptedException e) {
+				return null;
+			}
+		}
+		ArrayList<byte[]> ret;
+		synchronized (readbuffer) {
+			ret = new ArrayList<byte[]>(readbuffer);
+			readbuffer.clear();
+		}
+		return ret;
+	}
+
+	@Override
+	public void writePackets(ArrayList<byte[]> frames) throws IOException {
+		for(byte[] frame:frames){
+			phoenix.writePacket(frame);
+		}
+	}
+
+	@Override
+	public byte[] readPacket() {
+		while(readbuffer.isEmpty()){
+			try {
+				wait(10);
+			} catch (InterruptedException e) {
+				return null;
+			}
+		}
+		byte[] ret =readbuffer.get(0);
+		readbuffer.remove(0);
+		return ret;
+	}
+
+	@Override
+	public void writePacket(byte[] frame) throws IOException {
+		phoenix.writePacket(frame);
+		
+	}
+
+}
