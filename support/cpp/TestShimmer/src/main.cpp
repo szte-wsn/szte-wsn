71345cd42d8044d3f13917aec3d28770578f0c83
Peter Ruzicska <piitaj@gmail.com>
2010-08-02 20:49:12 +0000

    src maradek, tisztogatas
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@552 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/cpp/TestShimmer/src/main.cpp
+++ support/cpp/TestShimmer/src/main.cpp
@@ -1,13 +1,30 @@
 #include <QtGui/QApplication>
 #include "MainWindow.h"
 #include "Data3DPlot.h"
+#include "window.h"
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	MainWindow w;
+        MainWindow w;
 	w.show();
-        Plot plot;
-        plot.show();
+        //Plot plot;
+        //plot.show();
+        /*Window window;
+        window.resize(window.sizeHint());
+        int desktopArea = QApplication::desktop()->width() *
+                         QApplication::desktop()->height();
+        int widgetArea = window.width() * window.height();
+        if (((float)widgetArea / (float)desktopArea) < 0.75f)
+            window.show();
+        else
+            window.showMaximized();*/
+
 	return a.exec();
 }
+
+
+
+
+
