b03a0ee81302c4f02e0c13ba07b7fb381ecf9d1e
Sandor Nyilas <nyilasster@gmail.com>
2010-08-09 15:10:54 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@607 a4e16f3a-606c-4714-833f-ed604d7aca08

---
+++ support/java/org/szte/wsn/SnifferGraph/config/CreateConfGUI.java
@@ -0,0 +1,59 @@
+package config;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+/**
+ * 
+ * @author Németh Gábor
+ *	This class cares  if we want to create configuration XML file to be saved
+ *
+ */
+
+public class CreateConfGUI {
+	
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();			
+	
+	public final int screenHeight = screenSize.height;							
+	public final int screenWidth = screenSize.width;
+	
+	JFrame entity = new JFrame("Number's of...");
+	
+	JButton ok = new JButton("OK");
+	JButton cancel = new JButton("Cancel");
+	
+	JTextField input = new JTextField();
+	
+	public CreateConfGUI(){
+		entity.setLayout(null);
+		input.setBounds(30, 50, 150, 28);
+		ok.setBounds(190, 50, 75, 28);
+		cancel.setBounds(270, 50, 90, 28);
+		
+		entity.add(input);
+		entity.add(ok);
+		entity.add(cancel);
+		
+		ok.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+            	entity.setVisible(false);
+            	new CreatConfWindow(input.getText());
+            }
+		});
+		
+		cancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+            	entity.setVisible(false);
+            }
+		});
+		
+		entity.setSize(400, 150);
+		entity.setLocation((screenWidth/2)-200, (screenHeight/2)-75);
+		entity.setVisible(true);
+	}
+
+}
---
+++ support/java/org/szte/wsn/SnifferGraph/src/xmlReadWrite/WritingException.java
@@ -0,0 +1,15 @@
+package xmlReadWrite;
+/**
+ * 
+ * @author Nyilas Sandor Karoly , Nemeth Gabor
+ *this class  cares with writing exceptions
+ */
+public class WritingException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void XmlWritingException(java.lang.Throwable t) {
+	}
+}
