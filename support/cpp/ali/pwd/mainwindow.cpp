374e9106fb746a26efe58821d70b205996115860
Ali Baharev <ali.baharev@gmail.com>
2010-11-09 22:25:38 +0000

    Changing the working directory of the running process
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@1023 a4e16f3a-606c-4714-833f-ed604d7aca08

---
+++ support/cpp/ali/pwd/mainwindow.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "QDir"
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::changeEvent(QEvent *e)
+{
+    QMainWindow::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        cout << "PWD: " << QDir::currentPath().toStdString() << endl;
+        break;
+    }
+}
---
+++ support/cpp/ali/pwd/pwd.pro
@@ -0,0 +1,16 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-11-09T22:17:33
+#
+#-------------------------------------------------
+
+TARGET = pwd
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
