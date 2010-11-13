de26ff2a3cab5734c1b254c68538bc972e3568df
Peter Ruzicska <piitaj@gmail.com>
2010-11-13 10:13:29 +0000

    no message
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@1067 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/cpp/TestShimmer/src/SDownloadWidget.cpp
+++ support/cpp/TestShimmer/src/SDownloadWidget.cpp
@@ -1,6 +1,7 @@
 #include "SDownloadWidget.h"
 #include "Application.h"
 #include "ui_SDownloadWidget.h"
+#include <QFileDialog>
 
 SDownloadWidget::SDownloadWidget(QWidget *parent, Application &app) :
         QWidget(parent),
@@ -21,5 +22,8 @@ void SDownloadWidget::on_clearButton_clicked()
 
 void SDownloadWidget::on_downloadButton_clicked()
 {
-
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
+                                                     "/home",
+                                                     QFileDialog::ShowDirsOnly
+                                                     | QFileDialog::DontResolveSymlinks);
 }
