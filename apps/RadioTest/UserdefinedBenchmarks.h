333a3a302db65ebd8dbd5cb998f8e65c02402851
Krisztian Veress <veresskrisztian@gmail.com>
2010-12-13 14:50:19 +0000

    non-standard tests moved to Userdefined file, new benchmarks added
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/branches/extra_functionality@1310 a4e16f3a-606c-4714-833f-ed604d7aca08

--- apps/RadioTest/UserdefinedBenchmarks.h
+++ apps/RadioTest/UserdefinedBenchmarks.h
@@ -48,27 +48,131 @@
  *                   (the edge ids count from zero in the current benchmark)
  *
  *
- *  For examples, see below, and do not hesitate to erase them to reduce memory overhead.
+ *  For examples, see the demo benchmarks below.
+ *  These benchmarks are only for demo and reference purposes, so do not hesitate to erase them / comment them out to reduce memory overhead.
  */
+ 
+ 
+// Send 10 messages (Mote1 -> Mote 2) when the test starts, and that's it.
+_BMARK_START_
+  { 1, 2, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(10), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+// Send 10 messages (Mote2 -> Mote 1) when the test starts, and that's it.
+_BMARK_START_
+  { 2, 1, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(10), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+/* Send 10 broadcast messages when the test starts.
+ * Note: try this benchmark with different motecount options on the PC side
+ *  - if motecount is set to 1 (default for this benchmark), no reception is seen in receiver side stats,
+ *  - if motecount is set to 2 (-mc 2): 10 reception (Mote 2 is now present, hearing Mote 1),
+ *  - if motecount is set to 5 (-mc 5): 50 reception (Mote 2,3,4,5 are present, hearing Mote 1),
+ *  - ...
+ */
+_BMARK_START_
+  { 1, ALL, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(10), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+// Send 10 messages when the test starts, and request acks.
+_BMARK_START_
+  { 1, 2, NO_TIMER , { SEND_ON_INIT,  0, NEED_ACK, 0, 0 }, NUM(10), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+// Send a message and request ack for it. If not acked, fallback at most 5 times.
+_BMARK_START_
+  { 1, 2, NO_TIMER , { SEND_ON_INIT, STOP_ON_ACK, NEED_ACK, 0, 0 }, NUM(5), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+// Mote 1 sends 3 messages to Mote 2.
+// Mote 2 sends messages to Mote1, stops when ack received and sends at most 7 messages if no ack received.
+_BMARK_START_
+  { 1, 2, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(3), NO_REPLY, START_MSG_ID },
+  { 2, 1, NO_TIMER , { SEND_ON_INIT,  STOP_ON_ACK, NEED_ACK, 0, 0 }, NUM(7), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+// Start sending continously messages when the test starts. Message sending stops when the test stops.
+_BMARK_START_
+  { 1, 2, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(INFINITE), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+/**
+ * Mote 1 starts contin. sending msgs to Mote 2 when the test starts. (1st edge)
+ * Also Mote 1 is sending cont. broadcast msgs. (2nd edge)
+ * Mote 3 sends at most 100 messages to Mote 1, request acks, and if it receives an ack, stops. (3rd edge)
+ *
+ * Note that this way the broadcast messages (2nd edge) are heared by Mote 2 and Mote 3, so the receiver side
+ * statistics will be the double of the sender side ones on the 2nd edge. (Since every broadcast message sent by Mote 1 is heared by two motes!)
+ */
+_BMARK_START_
+  { 1, 2  , NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(INFINITE), NO_REPLY, START_MSG_ID },
+  { 1, ALL, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(INFINITE), NO_REPLY, START_MSG_ID },
+  { 3, 1  , NO_TIMER , { SEND_ON_INIT,  STOP_ON_ACK, 0, 0, 0 }, NUM(100), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+// Send one message on every timer tick. Timer1 is used.
+_BMARK_START_
+  { 1, 2, {TIMER(1),0} , { SEND_ON_TIMER,  0, 0, 0, 0 }, NUM(1), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+/* 1st edge: Send at most 10 messages on every timer tick. If ack received, stop sending. Timer1 is used.
+ * 2nd edge: Send one broadcast message to every node when Timer2 tickens.
+ */
+_BMARK_START_
+  { 1, 2, {TIMER(1),0} , { SEND_ON_TIMER,  STOP_ON_ACK, 0, 0, 0 }, NUM(10), NO_REPLY, START_MSG_ID },
+  { 1, ALL, {TIMER(2),0} , { SEND_ON_TIMER,  0, 0, 0, 0 }, NUM(1), NO_REPLY, START_MSG_ID }
+_BMARK_END_
 
 
+// 10
+/* Start cont. sending messages on every timer tick of Timer1, and stop sending if Timer2 tickens.
+ * By changing the type (oneshot/periodic) and frequency of the timers, different traffic patterns are 
+ * likely to be generated.
+ */
+_BMARK_START_  
+  { 1, 2, {TIMER(1),TIMER(2)} , { SEND_ON_TIMER,  STOP_ON_TIMER, 0, 0, 0 }, NUM(INFINITE), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+/* Mote 1 sends 3 messages on every timer tick of Timer1.
+ * Mote 2 starts cont. sending messages when the test starts and stops it if Timer2 tickens 
+ * or receives an ack, whichever comes first.
+ */
+_BMARK_START_
+  { 1, 2, {TIMER(1), 0} , { SEND_ON_TIMER,  0, 0, 0, 0 }, NUM(3), NO_REPLY, START_MSG_ID },
+  { 2, 1, {0,TIMER(2)} , { SEND_ON_INIT,  STOP_ON_TIMER | STOP_ON_ACK , 0, 0, 0 }, NUM(INFINITE), NO_REPLY, START_MSG_ID }
+_BMARK_END_
+
+/* Mote 1 sends 2 messages to Mote 2 on every Timer1 ticks. It stops (only sends one message) if Mote 2
+ * acknowledges the message. 
+ * If Mote 2 hears a message, it replies on edge 1 ( see REPLY_ON(1) of the 1st edge ), which means it 
+ * will send one message to Mote 3 (2nd edge). 
+ * Since REPLY_ON(2) is present in the 2nd edge, every time Mote 3 hears a message, it should reply on the 
+ * 3rd edge: sends one message to Mote 1.
+ */
 _BMARK_START_
   { 1, 2,   {TIMER(1),0}, { SEND_ON_TIMER,  STOP_ON_ACK, 0, 0, 0 }, NUM(2), REPLY_ON(1), START_MSG_ID },
   { 2, 3,   NO_TIMER , { SEND_ON_REQ,  0, 0, 0, 0 }, NUM(1), REPLY_ON(2), START_MSG_ID },
   { 3, 1,   NO_TIMER , { SEND_ON_REQ,  0, 0, 0, 0 }, NUM(1), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
+/* 1st edge : Mote 1 -> Mote 2: Exactly 2 messages on Timer1 ticks, request acks.
+ *  - every time Mote 2 hears a message from this edge, it should reply on the 3rd edge ( see REPLY_ON(2) )
+ * 2nd edge : Mote 3 -> Mote 2: One message on every Timer2 ticks, stop either on Timer3 ticks or on acks. 
+ *  - note that in this case the STOP_ON_X policies are useless, since
+ *    on this edge only one message is to be sent, so no use to 'stop' it...
+ *  - every time Mote 2 hears a message from this edge, it should reply on the 3rd edge ( see REPLY_ON(2) )
+ * 3rd edge : Mote 2 broadcasts exactly one message.
+ *  - if anyone (Mote 1,Mote 3) hears it, it should reply on the 4th edge ( REPLY_ON(3) ). Since the 4th edge's
+ *    sender is 3, this only applies for Mote 3.
+ * 4th edge : Mote 3 -> Mote 1: Exactly 4 messages to transmit.
+ *  - note that this edge has SEND_ON_INIT, so 4 messages are also transmitted when the test starts,
+ *    not just when Mote 3 replies for messages it gets on the 3rd edge! 
+ */
 _BMARK_START_
   { 1, 2,   {TIMER(1),0}, { SEND_ON_TIMER, 0, NEED_ACK, 0, 0 }, NUM(2), REPLY_ON(2), START_MSG_ID },
   { 3, 2,   {TIMER(2),TIMER(3)}, { SEND_ON_TIMER, STOP_ON_TIMER | STOP_ON_ACK, 0, 0, 0 }, NUM(1), REPLY_ON(2), START_MSG_ID },
   { 2, ALL, NO_TIMER    , { SEND_ON_REQ,  0, 0, 0, 0 }, NUM(1), REPLY_ON(3), START_MSG_ID },
   { 3, 1,   NO_TIMER    , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(4) , NO_REPLY, START_MSG_ID }
-_BMARK_END_
-
-
-_BMARK_START_
-  { 1, 2,   {TIMER(1),0}, { SEND_ON_TIMER,  STOP_ON_ACK, NEED_ACK, 0, 0 }, NUM(2), REPLY_ON(1), START_MSG_ID },
-  { 2, 1,   NO_TIMER , { SEND_ON_REQ,  0, 0, 0, 0 }, NUM(2), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
 
