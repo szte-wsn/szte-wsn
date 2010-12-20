f24966414885b16a6974e711f654f1c4cbb24ba8
Krisztian Veress <veresskrisztian@gmail.com>
2010-12-20 17:34:07 +0000

    new benchmark identification support, more docs added
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/branches/extra_functionality/apps/RadioTest@1396 a4e16f3a-606c-4714-833f-ed604d7aca08

--- UserdefinedBenchmarks.h
+++ UserdefinedBenchmarks.h
@@ -5,8 +5,11 @@
  * This is a user-modifiable file, keep it clean, and stay to the rules below.
  *
  * Instructions for how to define a benchmark:
- *  1, All benchmarks MUST begin with a _BMARK_START_ macro.
- *  2, All benchmarks MUST end   with a _BMARK_END_   macro.
+ *  1, All benchmarks MUST begin with a _BMARK_START_(X) macro, where X is its unique identifier.
+ *     Note that if multiple benchmarks have the same id, only the first is seen by the program,
+ *     others are ignored -- thus only eating expensive memory.
+ *     Valid ids are from [1000,...,65535]
+ *  2, All benchmarks MUST end with a _BMARK_END_ macro.
  *  3, Between these macros, the edges (allowed communication links between two)
  *     separate motes) of the modeled network are enlisted.
  *     
@@ -24,12 +27,12 @@
  *
  *     START_TIMER_DESC:
  *     STOP_TIMER_DESC:
- *                - '0', if sending is not initiated by a timer
- *                - 'TIMER(X)', representing the Xth timer.
+ *                - '0', if sending/stopping is not initiated by a timer
+ *                - 'TIMER(X)', representing the Xth timer, ex: TIMER(2)
  *     
  *     POLICY_DESC:
  *                - { SEND_TRIG, STOP_TRIG, ACK, 0, 0 }
- *     SEND_TRIG: - 'SEND_NONE', to send never, nothing,
+ *     SEND_TRIG: - 'SEND_ON_REQ', to send only if implicitly required (see below)
  *                - 'SEND_ON_INIT', to send message on benchmark start,
  *                - 'SEND_ON_TIMER', to send message on timer event (
  *                  see START_TIMER_DESC)
@@ -40,26 +43,43 @@
  *     ACK:       - '0', if acknowledgements are not requested
  *                - 'NEED_ACK', if acknowledgements are requested
  *
- *     MSG_COUNT: - NUM(X), denoting X message to send.
+ *     MSG_COUNT: - NUM(X), denoting X message(s) to send, where X can be from [1,..,255].
+ *                - NUM(INFINITE), denoting continous message sending.
  *
  *     REPLY:     - 'NO_REPLY', if message is not required to send on reception
  *                - 'REPLY_EDGE(X)', if message is to send on reception on edge X.
- *                - 'REPLY_EDGE(X) | REPLY_EDGE(Y) | ...', if message is to send on reception on edge X and Y
+ *                - 'REPLY_EDGE(X) | REPLY_EDGE(Y) | ...', if message is to send on reception 
+ *                   on edge X AND on edge Y also.
  *                   (the edge ids count from zero in the current benchmark)
  *
+ * By specifying the edges, the required mote count is implicitly determined by the maximal mote id
+ * present either in the sender or receiver sections of the edge descriptions. (This can aslo be
+ * overridden with a command line option (-mc) of the PC program. )
  *
- *  For examples, see the demo benchmarks below.
- *  These benchmarks are only for demo and reference purposes, so do not hesitate to erase them / comment them out to reduce memory overhead.
+ * In the following example, the implied mote count is 1:
+ * _BMARK_START_(2034)
+ *  { 1, ALL, NO_TIMER , ... }
+ * _BMARK_END_
+ *
+ * However, if someone would like to increase this number (ex. to 4), there is a naughty trick:
+ * _BMARK_START_(2034)
+ *  { 4, ALL, NO_TIMER , ... }
+ * _BMARK_END_
+ *
+ * You are encouraged to use this motecount-force, rather than depending on the command-line option.
+ *
+ * For complete examples, see the demo benchmarks below.
+ * These benchmarks are only for demo and reference purposes, so do not hesitate to erase them / comment them out to reduce the memory overhead.
+ *  
  */
- 
- 
+
 // Send 10 messages (Mote1 -> Mote 2) when the test starts, and that's it.
-_BMARK_START_
+_BMARK_START_(1001)
   { 1, 2, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(10), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
 // Send 10 messages (Mote2 -> Mote 1) when the test starts, and that's it.
-_BMARK_START_
+_BMARK_START_(1002)
   { 2, 1, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(10), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
@@ -70,29 +90,29 @@ _BMARK_END_
  *  - if motecount is set to 5 (-mc 5): 50 reception (Mote 2,3,4,5 are present, hearing Mote 1),
  *  - ...
  */
-_BMARK_START_
+_BMARK_START_(1003)
   { 1, ALL, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(10), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
 // Send 10 messages when the test starts, and request acks.
-_BMARK_START_
+_BMARK_START_(1004)
   { 1, 2, NO_TIMER , { SEND_ON_INIT,  0, NEED_ACK, 0, 0 }, NUM(10), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
 // Send a message and request ack for it. If not acked, fallback at most 5 times.
-_BMARK_START_
+_BMARK_START_(1005)
   { 1, 2, NO_TIMER , { SEND_ON_INIT, STOP_ON_ACK, NEED_ACK, 0, 0 }, NUM(5), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
 // Mote 1 sends 3 messages to Mote 2.
 // Mote 2 sends messages to Mote1, stops when ack received and sends at most 7 messages if no ack received.
-_BMARK_START_
+_BMARK_START_(1006)
   { 1, 2, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(3), NO_REPLY, START_MSG_ID },
   { 2, 1, NO_TIMER , { SEND_ON_INIT,  STOP_ON_ACK, NEED_ACK, 0, 0 }, NUM(7), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
 // Start sending continously messages when the test starts. Message sending stops when the test stops.
-_BMARK_START_
+_BMARK_START_(1007)
   { 1, 2, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(INFINITE), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
@@ -104,32 +124,30 @@ _BMARK_END_
  * Note that this way the broadcast messages (2nd edge) are heared by Mote 2 and Mote 3, so the receiver side
  * statistics will be the double of the sender side ones on the 2nd edge. (Since every broadcast message sent by Mote 1 is heared by two motes!)
  */
-_BMARK_START_
+_BMARK_START_(1008)
   { 1, 2  , NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(INFINITE), NO_REPLY, START_MSG_ID },
   { 1, ALL, NO_TIMER , { SEND_ON_INIT,  0, 0, 0, 0 }, NUM(INFINITE), NO_REPLY, START_MSG_ID },
   { 3, 1  , NO_TIMER , { SEND_ON_INIT,  STOP_ON_ACK, 0, 0, 0 }, NUM(100), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
 // Send one message on every timer tick. Timer1 is used.
-_BMARK_START_
+_BMARK_START_(1009)
   { 1, 2, {TIMER(1),0} , { SEND_ON_TIMER,  0, 0, 0, 0 }, NUM(1), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
 /* 1st edge: Send at most 10 messages on every timer tick. If ack received, stop sending. Timer1 is used.
  * 2nd edge: Send one broadcast message to every node when Timer2 tickens.
  */
-_BMARK_START_
+_BMARK_START_(1010)
   { 1, 2, {TIMER(1),0} , { SEND_ON_TIMER,  STOP_ON_ACK, 0, 0, 0 }, NUM(10), NO_REPLY, START_MSG_ID },
   { 1, ALL, {TIMER(2),0} , { SEND_ON_TIMER,  0, 0, 0, 0 }, NUM(1), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
-
-// 10
 /* Start cont. sending messages on every timer tick of Timer1, and stop sending if Timer2 tickens.
  * By changing the type (oneshot/periodic) and frequency of the timers, different traffic patterns are 
  * likely to be generated.
  */
-_BMARK_START_  
+_BMARK_START_(1011)
   { 1, 2, {TIMER(1),TIMER(2)} , { SEND_ON_TIMER,  STOP_ON_TIMER, 0, 0, 0 }, NUM(INFINITE), NO_REPLY, START_MSG_ID }
 _BMARK_END_
 
@@ -137,7 +155,7 @@ _BMARK_END_
  * Mote 2 starts cont. sending messages when the test starts and stops it if Timer2 tickens 
  * or receives an ack, whichever comes first.
  */
-_BMARK_START_
+_BMARK_START_(1012)
   { 1, 2, {TIMER(1), 0} , { SEND_ON_TIMER,  0, 0, 0, 0 }, NUM(3), NO_REPLY, START_MSG_ID },
   { 2, 1, {0,TIMER(2)} , { SEND_ON_INIT,  STOP_ON_TIMER | STOP_ON_ACK , 0, 0, 0 }, NUM(INFINITE), NO_REPLY, START_MSG_ID }
 _BMARK_END_
@@ -149,7 +167,7 @@ _BMARK_END_
  * Since REPLY_ON(2) is present in the 2nd edge, every time Mote 3 hears a message, it should reply on the 
  * 3rd edge: sends one message to Mote 1.
  */
-_BMARK_START_
+_BMARK_START_(1013)
   { 1, 2,   {TIMER(1),0}, { SEND_ON_TIMER,  STOP_ON_ACK, 0, 0, 0 }, NUM(2), REPLY_ON(1), START_MSG_ID },
   { 2, 3,   NO_TIMER , { SEND_ON_REQ,  0, 0, 0, 0 }, NUM(1), REPLY_ON(2), START_MSG_ID },
   { 3, 1,   NO_TIMER , { SEND_ON_REQ,  0, 0, 0, 0 }, NUM(1), NO_REPLY, START_MSG_ID }
@@ -168,7 +186,7 @@ _BMARK_END_
  *  - note that this edge has SEND_ON_INIT, so 4 messages are also transmitted when the test starts,
  *    not just when Mote 3 replies for messages it gets on the 3rd edge! 
  */
-_BMARK_START_
+_BMARK_START_(1014)
   { 1, 2,   {TIMER(1),0}, { SEND_ON_TIMER, 0, NEED_ACK, 0, 0 }, NUM(2), REPLY_ON(2), START_MSG_ID },
   { 3, 2,   {TIMER(2),TIMER(3)}, { SEND_ON_TIMER, STOP_ON_TIMER | STOP_ON_ACK, 0, 0, 0 }, NUM(1), REPLY_ON(2), START_MSG_ID },
   { 2, ALL, NO_TIMER    , { SEND_ON_REQ,  0, 0, 0, 0 }, NUM(1), REPLY_ON(3), START_MSG_ID },
