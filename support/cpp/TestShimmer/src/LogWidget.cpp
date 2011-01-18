87ad77984d21a471930ab15aed534621feae677c
Ali Baharev <ali.baharev@gmail.com>
2011-01-18 11:25:39 +0000

    Removed Hungarian names; added copyright notice
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@1528 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/cpp/TestShimmer/src/LogWidget.cpp
+++ support/cpp/TestShimmer/src/LogWidget.cpp
@@ -1,3 +1,36 @@
+/** Copyright (c) 2010, University of Szeged
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* - Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* - Redistributions in binary form must reproduce the above
+* copyright notice, this list of conditions and the following
+* disclaimer in the documentation and/or other materials provided
+* with the distribution.
+* - Neither the name of University of Szeged nor the names of its
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+* OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Péter Ruzicska
+*/
+
 #include "LogWidget.h"
 #include "ui_LogWidget.h"
 #include "Application.h"
@@ -54,16 +87,16 @@ void LogWidget::createItem(QString text)
     ui->log->setCellWidget(row-1,0,but);
 
     QDateTimeEdit *dateEdit = new QDateTimeEdit(QDate::currentDate());
-     dateEdit->setMinimumDate(QDate::currentDate().addDays(-365));
-     dateEdit->setMaximumDate(QDate::currentDate().addDays(365));
-     dateEdit->setCalendarPopup(true);
-     //dateEdit->setReadOnly(true);
-     dateEdit->setDisplayFormat("yyyy.MM.dd");
+    dateEdit->setMinimumDate(QDate::currentDate().addDays(-365));
+    dateEdit->setMaximumDate(QDate::currentDate().addDays(365));
+    dateEdit->setCalendarPopup(true);
+    //dateEdit->setReadOnly(true);
+    dateEdit->setDisplayFormat("yyyy.MM.dd");
     ui->log->setCellWidget(row-1, 1, dateEdit);
     ui->log->cellWidget(row-1, 1)->setEnabled(false);
 
     QDateTimeEdit *time = new QDateTimeEdit(QTime::currentTime());
-     //time->setReadOnly(true);
+    //time->setReadOnly(true);
     ui->log->setCellWidget(row-1, 2, time);
     ui->log->cellWidget(row-1, 2)->setEnabled(false);
 
@@ -71,7 +104,7 @@ void LogWidget::createItem(QString text)
     ui->log->setItem(row-1,3,item);
 
     QPushButton* del = new QPushButton(QIcon(":/icons/Delete.png"),"",this);
-     del->setMaximumSize(20,20);
+    del->setMaximumSize(20,20);
     ui->log->setCellWidget(row-1,4,del);
     signalMapper->setMapping(del, row-1);
 
@@ -99,33 +132,33 @@ void LogWidget::setTableEditable(bool isEditable)
     }
 }
 
-void LogWidget::on_felvKezdButton_clicked()
+void LogWidget::on_recStartButton_clicked()
 {
-    createItem(QString::fromUtf8("Felvétel Kezdete"));
+    createItem(QString::fromUtf8("Rec start"));
 }
 
-void LogWidget::on_felvVegButton_clicked()
+void LogWidget::on_recEndButton_clicked()
 {
-    createItem(QString::fromUtf8("Felvétel Vége"));
+    createItem(QString::fromUtf8("Rec end"));
 }
 
-void LogWidget::on_mozgKezdButton_clicked()
+void LogWidget::on_motionStartButton_clicked()
 {
-    createItem(QString::fromUtf8("Mozgás Kezdete"));
+    createItem(QString::fromUtf8("Motion start"));
 }
 
-void LogWidget::on_mozgVegButton_clicked()
+void LogWidget::on_motionEndButton_clicked()
 {
-    createItem(QString::fromUtf8("Mozgás Vége"));
+    createItem(QString::fromUtf8("Motion end"));
 }
 
 void LogWidget::onDelRow(int row)
 {
     QMessageBox msgBox;
-     msgBox.setText("DELETE");
-     msgBox.setInformativeText("Are you sure you want to delete this row?");
-     msgBox.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
-     msgBox.setDefaultButton(QMessageBox::Cancel);
-     int ret = msgBox.exec();
-    if(ret == QMessageBox::Ok) ui->log->removeRow(row);
+    msgBox.setInformativeText("Are you sure you want to delete this row?");
+    msgBox.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+    msgBox.setDefaultButton(QMessageBox::Cancel);
+    int ret = msgBox.exec();
+    if(ret == QMessageBox::Ok)
+        ui->log->removeRow(row);
 }
