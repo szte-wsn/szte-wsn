63d2770906e5d8d3ffad6ff21b8b080ec56c60f9
Ali Baharev <ali.baharev@gmail.com>
2011-02-11 22:26:02 +0000

    Mock person implementation
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@1753 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/cpp/TestShimmer/src/LogWidget.cpp
+++ support/cpp/TestShimmer/src/LogWidget.cpp
@@ -51,6 +51,30 @@
 #include <QFontMetrics>
 #include "GLWindow.hpp"
 
+class Person {
+
+public:
+
+    Person() : ID(-1), Name("not set"), born(QDate()) { }
+
+    Person(qint64 id, const QString& name, const QDate& birth) : ID(id), Name(name), born(birth) { }
+
+    qint64 id() const { return ID; }
+
+    const QString name() const { return Name; }
+
+    const QDate birth() const { return born; }
+
+private:
+
+    qint64 ID;
+
+    QString Name;
+
+    QDate born;
+
+};
+
 namespace {
 
     const int NO_MORE = -1;
