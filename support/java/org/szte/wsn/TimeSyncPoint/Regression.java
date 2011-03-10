6caf9b0d777be6bb66cc23f233c69aee52171b77
Andras Biro <bbandi86@gmail.com>
2011-03-10 13:17:47 +0000

    Documentation; some minor bugfix
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@1876 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/java/org/szte/wsn/TimeSyncPoint/Regression.java
+++ support/java/org/szte/wsn/TimeSyncPoint/Regression.java
@@ -1,7 +1,45 @@
+/*
+* Copyright (c) 2010, University of Szeged
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* - Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* - Redistributions in binary form must reproduce the above
+* copyright notice, this list of conditions and the following
+* disclaimer in the documentation and/or other materials provided
+* with the distribution.
+* - Neither the name of University of Szeged nor the names of its
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+* OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Author:Andras Biro
+*/
 package org.szte.wsn.TimeSyncPoint;
 
 import java.util.ArrayList;
 
+/**
+ * Calculates linear regression from 2 dimensional points
+ * @author Andras Biro
+ *
+ */
 public class Regression {
 	private ArrayList<Point> points=new ArrayList<Point>();
 	private boolean functionNeedsUpdate=true;
@@ -24,20 +62,51 @@ public class Regression {
 		}
 	}
 	
+	/**
+	 * Default constructor
+	 * @param maxerror Maximum distance of a new point from the already calculated line
+	 * @param defaultslope Default slope of the function (if there's only one point)
+	 */
 	public Regression(long maxerror, double defaultslope){
 		this.maxerror=maxerror;
 		this.defaultslope=defaultslope;
 	}	
 	
+	/**
+	 * Returns maximum distance of a new point from the already calculated line
+	 * @return Maximum distance
+	 */
 	public long getMaxerror() {
 		return maxerror;
 	}
 
-	public void setMaxerror(long maxerror, double defaultslope) {
+	/**
+	 * Sets the maximum distance of a new point from the already calculated line
+	 * @param maxerror Maximum distance
+	 */
+	public void setMaxerror(long maxerror) {
 		this.maxerror = maxerror;
-		this.defaultslope = defaultslope;
 	}
 	
+	/**
+	 * Sets the default slope of the function
+	 * @param defaultslope Default slope of the function (if there's only one point)
+	 */
+	public void setDefaultSlope(double defaultslope) {
+		this.defaultslope = defaultslope;
+	}
+
+	/**
+	 * Returns the default slope of the function
+	 * @return Default slope of the function (if there's only one point)
+	 */
+	public double getDefaultslope() {
+		return defaultslope;
+	}
+	
+	/**
+	 * @return Number of points in the regression
+	 */
 	public long getNumPoints() {
 		return points.size();
 	}
@@ -57,6 +126,12 @@ public class Regression {
 		}
 	}
 	
+	/**
+	 * Adds a point into the regression if it's not too far from the line
+	 * @param x X coordinate
+	 * @param y Y coordinate
+	 * @return false if the point is farer from the line than maxerror, true otherwise
+	 */
 	public boolean addPoint(long x,long y){
 		Point p=new Point(x,y);
 		if(calcError(p)<maxerror){
@@ -67,13 +142,19 @@ public class Regression {
 			return false;
 	}
 	
-
-
+	/**
+	 * Adds a point into the regression
+	 * @param x X coordinate
+	 * @param y Y coordinate
+	 */
 	public void addPointNoVerify(long x,long y){
 		points.add(new Point(x,y));
 		functionNeedsUpdate=true;
 	}
 	
+	/**
+	 * @return The calculated functionSSS
+	 */
 	public LinearFunction getFunction(){
 		if(functionNeedsUpdate){
 			calculateFunction();
@@ -109,13 +190,7 @@ public class Regression {
 		return function;
 	}
 
-	public void setDefaultslope(double defaultslope) {
-		this.defaultslope = defaultslope;
-	}
 
-	public double getDefaultslope() {
-		return defaultslope;
-	}
 	
 	
 }
