db12435cf290c6b77bf47f8340b4cf763d2576a9
Miklos Toth <tothmiklos86@gmail.com>
2011-03-30 23:39:45 +0000

    UI added, according to the ReadMe.txt
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@2037 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/java/org/szte/wsn/CSVProcess/CSVProcess.java
+++ support/java/org/szte/wsn/CSVProcess/CSVProcess.java
@@ -43,30 +43,47 @@ import org.szte.wsn.TimeSyncPoint.LinearFunction;
 import org.szte.wsn.TimeSyncPoint.TSParser;
 
 public class CSVProcess{
-	
-	private String separator=";";
-	private String nodeIdSeparator=":";
-	private File avgOutputFileName =new File("avgfile.csv");	
-	private boolean insertGlobal=true;	
-	private int maxError=120;	
-	private String timeFormat="yyyy.MM.dd/HH:mm:ss.SSS";	
-	private String confFile="convert.conf";	
-	private String csvExt=".csv";		
-	private long startTime=Long.MIN_VALUE;
-	private long endTime=Long.MAX_VALUE;	
-	private long timeWindow=900000;	
-	private byte timeType=CSVHandler.TIMETYPE_START;
-	
-	private int localColumn[]={4}; //time *
-	private int globalColumn[]={5}; //time *
-	private File outputFile=new File("global.csv"); //*
-	private ArrayList<Integer> dataColumns[];  // *
 
+	private static String separator=";";
+	private static String nodeIdSeparator=":";
+	private static int maxError=120;	
+	private static String timeFormat="yyyy.MM.dd/HH:mm:ss.SSS";	
+	private static String confFile="convert.conf";	
+	private static String csvExt=".csv";		
+	private static long startTime=Long.MIN_VALUE;
+	private static long endTime=Long.MAX_VALUE;	
+	private static long timeWindow=900000;	
+	private static byte timeType=CSVHandler.TIMETYPE_START;
+
+	//private ArrayList<StructParams> structures;
+	//TODO local variables
+	private ArrayList<Integer>[] datacolumns;
+	private File outputFile;
+	private File avgOutputFileName;
+	private int globalColumn[];
+	private boolean insertGlobal;
+	private int localColumn[];
+	
 	private int runningConversions;
 	public ArrayList<CSVHandler> filesPerNode[]=null;	
 
-	public CSVProcess(ArrayList<String> inputFiles, ArrayList<Integer> dataColumns[]){
-		this.dataColumns=dataColumns;
+	@SuppressWarnings("unchecked")
+	public CSVProcess(ArrayList<String> inputFiles, ArrayList<StructParams> structs){
+		
+		datacolumns=new ArrayList[structs.size()];
+		
+			
+		outputFile=structs.get(0).getOutputFile();
+		avgOutputFileName=structs.get(0).getAvgOutputFile();
+		globalColumn=new int[structs.size()];
+		localColumn=new int[structs.size()];
+		for(int i=0;i<structs.size();i++){
+			globalColumn[i]=structs.get(i).getGlobalColumn();
+			localColumn[i]=structs.get(i).getLocalColumn();
+			datacolumns[i]=structs.get(i).getDataColumns();
+			
+		}
+		this.insertGlobal=structs.get(0).isInsertGlobal();
 		try {
 			this.outputFile.createNewFile();
 		} catch (IOException e) {
@@ -77,87 +94,146 @@ public class CSVProcess{
 		for(String file:inputFiles)
 			new Converter(file, confFile, csvExt, separator, new PerConversion());
 	}
-	private void initParams(String fileName){
 
+
+	private static ArrayList<StructParams> initParams(String fileName){
+
+		ArrayList<StructParams> structs=new ArrayList<StructParams>();
 		try {
 			BufferedReader input =  new BufferedReader(new FileReader(fileName));
 			try {
+
+				int localColumn =-1;
+				int globalColumn =-1;
+				int[] dataColumns=null;
+				String outputName="global";
+				String avgName="avg";
+				boolean insertGlobal = true; 
+				String name=null;
+
 				String line = null; 
 				String keyWord=null;
 				String value=null;
+				boolean global=true;
+
 				while (( line = input.readLine()) != null){
 					if (line.contains("=")){
 						keyWord=line.split("=")[0];
-						keyWord=keyWord.trim();      
+						keyWord=keyWord.trim();      //removing whitespace from the keyword
 						value=line.split("=")[1];
 						value=value.trim();
 						if (value.startsWith("\""))
 							value=value.substring(1, value.length());  
 						if (value.endsWith("\""))            //removing " characters separately in case someone misses the end 
 							value=value.substring(0, value.length()-1); 						
-						
-						
-						if(keyWord.equalsIgnoreCase("separator")) 
-							separator=value;
-						if(keyWord.equalsIgnoreCase("nodeIdSeparator"))
-							nodeIdSeparator=value;
-						if(keyWord.equalsIgnoreCase("avgOutputFileName")) 
-							avgOutputFileName=new File(value);
-						if(keyWord.equalsIgnoreCase("insertGlobal")) 
-							insertGlobal=value.equalsIgnoreCase("true");
-						if(keyWord.equalsIgnoreCase("maxError")) 
-							maxError=Integer.parseInt(value);
-						if(keyWord.equalsIgnoreCase("timeFormat")) 
-							timeFormat=value;						
-						if(keyWord.equalsIgnoreCase("confFile")) 
-							confFile=value;
-						if(keyWord.equalsIgnoreCase("csvExt")) 
-							csvExt=value;						
-						if(keyWord.equalsIgnoreCase("startTime")) {
-							if (value.equalsIgnoreCase("min"))
-								startTime=Long.MIN_VALUE;
-							else
-							startTime=Long.parseLong(value);
+						if(global){
+							if(keyWord.equalsIgnoreCase("separator")) 
+								separator=value;
+							if(keyWord.equalsIgnoreCase("nodeIdSeparator"))
+								nodeIdSeparator=value;
+							if(keyWord.equalsIgnoreCase("maxError")) 
+								maxError=Integer.parseInt(value);
+							if(keyWord.equalsIgnoreCase("timeFormat")) 
+								timeFormat=value;						
+							if(keyWord.equalsIgnoreCase("confFile")) 
+								confFile=value;
+							if(keyWord.equalsIgnoreCase("csvExt")) 
+								csvExt=value;						
+							if(keyWord.equalsIgnoreCase("startTime")) {
+								if (value.equalsIgnoreCase("min"))
+									startTime=Long.MIN_VALUE;
+								else
+									startTime=Long.parseLong(value);
 							}
-						if(keyWord.equalsIgnoreCase("endTime")) {
-							if (value.equalsIgnoreCase("max"))
-								endTime=Long.MAX_VALUE;
-							else
-							endTime=Long.parseLong(value);
+							if(keyWord.equalsIgnoreCase("endTime")) {
+								if (value.equalsIgnoreCase("max"))
+									endTime=Long.MAX_VALUE;
+								else
+									endTime=Long.parseLong(value);
 							}			
-						
-						if(keyWord.equalsIgnoreCase("timeWindow")) 
-							timeWindow=Long.parseLong(value);
-						
-						if(keyWord.equalsIgnoreCase("timeType")) {
-							if (value.equalsIgnoreCase("middle"))
-								timeType=CSVHandler.TIMETYPE_MIDDLE;
-							else if (value.equalsIgnoreCase("end"))
-								timeType=CSVHandler.TIMETYPE_END;
-							else if (value.equalsIgnoreCase("start"))
-								timeType=CSVHandler.TIMETYPE_START;
-							else
-							System.out.println("WARNING: Could not identify timeType during the parsing of "+fileName +"CSVProcess config file. ");
-							}	//end of global parameter parsing
-						
 
-						
+							if(keyWord.equalsIgnoreCase("timeWindow")) 
+								timeWindow=Long.parseLong(value);
+
+							if(keyWord.equalsIgnoreCase("timeType")){ 
+								if (value.equalsIgnoreCase("middle"))
+									timeType=CSVHandler.TIMETYPE_MIDDLE;
+								else if (value.equalsIgnoreCase("end"))
+									timeType=CSVHandler.TIMETYPE_END;
+								else if (value.equalsIgnoreCase("start"))
+									timeType=CSVHandler.TIMETYPE_START;
+								else
+									System.out.println("WARNING: Could not identify timeType during the parsing of "+fileName +"CSVProcess config file. ");
+							}
+						}//end of global parameter parsing branch
+						else{
+							if(keyWord.equalsIgnoreCase("avgOutputFileName")) 
+								avgName=value;
+							if(keyWord.equalsIgnoreCase("insertGlobal")) 
+								insertGlobal=value.equalsIgnoreCase("true");
+							if(keyWord.equalsIgnoreCase("outputFileName"))
+								outputName=value;
+
+							try {
+								if(keyWord.equalsIgnoreCase("localColumn"))
+									localColumn=Integer.parseInt(value);
+								if(keyWord.equalsIgnoreCase("globalColumn"))
+									globalColumn=Integer.parseInt(value);
+								if(keyWord.equalsIgnoreCase("dataColumns")){
+									String[] values=value.split(",");
+									dataColumns=new int[values.length];
+									for(int i=0; i<values.length;i++)
+										dataColumns[i]=Integer.parseInt(values[i]);									
+								}
+
+							} catch (NumberFormatException e) {
+								System.out.println("WARNING: Could not parse columns indicators during the parsing of "+fileName +" CSVProcess config file. ");
+								e.printStackTrace();
+							}
+						}
 					}
-				}
+					else if(line.contains("structure")){
+						if(global)
+							global=false;
+						else
+						{							
+							localColumn=-1;
+							globalColumn=-1;
+							dataColumns=null;
+						}
+						if (line.contains(" ")){
+							value=line.split(" ")[1];
+							name=value;
+						}
+						else
+							name=null;
+
+
+					}	
+					else if(line.contains("endofstruct")){
+						if((localColumn<0)||(globalColumn<0)||(dataColumns==null)||(name==null)){
+							System.err.println("ERROR: Missing parameters while parsing "+fileName +" CSVProcess config file, "+name+" struct.");
+							System.err.println("LocalColumn, globalColumn, datacolumns and name are mandatory for every structure");
+							System.exit(1);
+						}
+						structs.add(new StructParams(localColumn, globalColumn, dataColumns, outputName, avgName, insertGlobal, name));
+					}
+				}//end of while 
+
 			}
 			finally {
 				input.close();
 			}
 		}
 		catch(NumberFormatException e){
-			System.out.println("ERROR, could not parse every parameters of "+fileName +"CSVProcess config file. ");
+			System.out.println("ERROR, could not parse every parameters of "+fileName +" CSVProcess config file. ");
 		}
 		catch (IOException e) {			
 			System.out.println("IO ERROR while working with: "+fileName);
 			e.printStackTrace();
 			System.exit(1);			
 		}
-
+		return structs;
 	}
 
 	private void mergeConversion(ArrayList<CSVHandler> fileGroup) {
@@ -203,7 +279,7 @@ public class CSVProcess{
 		public void Ready(Converter output) {
 			CSVHandler[] ready=null;
 			try {
-				ready = output.toCSVHandlers(localColumn, dataColumns);
+				ready = output.toCSVHandlers(localColumn, datacolumns);
 			} catch (IOException e) {
 				System.out.println("Can't open parsed file");
 			} 
@@ -232,32 +308,23 @@ public class CSVProcess{
 
 	}
 
-	@SuppressWarnings("unchecked")
+
 	public static void main(String[] args){
 		String[] fileNames=new File(".").list();
-		//		boolean convertToSI=true;
-		//		long avgWindow=900000;
-		//		int number=15;
-		//		Long startTime=null;
-		//		Long endTime=startTime;
-		//		String separator=",";
-		ArrayList<Integer>[] datacolumns=new ArrayList[1];
-		datacolumns[0]=new ArrayList<Integer>();
-		datacolumns[0].add(1);
-		datacolumns[0].add(2);
-		datacolumns[0].add(3);
-		datacolumns[0].add(4);
-		File outputfile=new File("global.csv");
-		//		String timef=null;//"yyyy.MM.dd. HH:mm:ss.SSS";
-		//		String globaln="globaltime";
-		outputfile.delete();
 		ArrayList<String> inputfiles=new ArrayList<String>();
 		for(String file:fileNames){
 			if(file.endsWith(".bin")){
 				inputfiles.add(file);
 			}
 		}
-		new CSVProcess(inputfiles,datacolumns);
+		String initFileName=(args.length>0)?args[0]:"ini.conf";
+		ArrayList<StructParams> structs= initParams(initFileName);		
+		
+		File outputfile=structs.get(0).getOutputFile(); //TODO
+		outputfile.delete();
+		
+
+		new CSVProcess(inputfiles,structs);
 
 	}
 
