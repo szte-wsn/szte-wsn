9c3c5b0dbf81258ffc0482da91bc298c8d642f0b
Andras Biro <bbandi86@gmail.com>
2011-03-31 10:23:56 +0000

    added thread lister for win debug
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@2044 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/java/org/szte/wsn/downloader2/StreamDownloader.java
+++ support/java/org/szte/wsn/downloader2/StreamDownloader.java
@@ -299,7 +299,42 @@ public class StreamDownloader{
 				System.out.println("Unimplemented command.");
 			}
 		}
+		// Find the root thread group
+		ThreadGroup root = Thread.currentThread().getThreadGroup().getParent();
+		while (root.getParent() != null) {
+		    root = root.getParent();
+		}
+
+		// Visit each thread group
+		visit(root, 0);
 		System.out.println("5");
+	}
+	
+	
+
+	// This method recursively visits all thread groups under `group'.
+	public static void visit(ThreadGroup group, int level) {
+	    // Get threads in `group'
+	    int numThreads = group.activeCount();
+	    Thread[] threads = new Thread[numThreads*2];
+	    numThreads = group.enumerate(threads, false);
+
+	    // Enumerate each thread in `group'
+	    for (int i=0; i<numThreads; i++) {
+	        // Get thread
+	        Thread thread = threads[i];
+	        System.out.println(thread.getName());
+	    }
+
+	    // Get thread subgroups of `group'
+	    int numGroups = group.activeGroupCount();
+	    ThreadGroup[] groups = new ThreadGroup[numGroups*2];
+	    numGroups = group.enumerate(groups, false);
+
+	    // Recursively visit each subgroup
+	    for (int i=0; i<numGroups; i++) {
+	        visit(groups[i], level+1);
+	    }
 	}
 
 	private static DataWriter getWriter(int nodeid, ArrayList<DataWriter> datawriters ){
