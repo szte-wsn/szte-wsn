1d9838d4a268a52e389468bc6fb6b90827fb1dbe
Andras Biro <bbandi86@gmail.com>
2011-03-31 10:31:33 +0000

    removed debug lines
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@2045 a4e16f3a-606c-4714-833f-ed604d7aca08

--- support/java/org/szte/wsn/downloader2/StreamDownloader.java
+++ support/java/org/szte/wsn/downloader2/StreamDownloader.java
@@ -282,59 +282,19 @@ public class StreamDownloader{
 				System.out.println("Are you sure you want to quit?");
 				String chr=console.readChar(new String[]{"y", "n"});
 				if(chr.equals("y")){
-					System.out.println("1");
 					for(DataWriter writer:writers)
 						try {
 							writer.close();
 						} catch (IOException e) {
 							System.err.println("Can't close file of nodeid: "+writer.getNodeid());
 						}
-					System.out.println("2");
 					comm.stop();
-					System.out.println("3");
 					exitCommandLoop=true;
-					System.out.println("4");
 				}
 			} else {
 				System.out.println("Unimplemented command.");
 			}
 		}
-		// Find the root thread group
-		ThreadGroup root = Thread.currentThread().getThreadGroup().getParent();
-		while (root.getParent() != null) {
-		    root = root.getParent();
-		}
-
-		// Visit each thread group
-		visit(root, 0);
-		System.out.println("5");
-	}
-	
-	
-
-	// This method recursively visits all thread groups under `group'.
-	public static void visit(ThreadGroup group, int level) {
-	    // Get threads in `group'
-	    int numThreads = group.activeCount();
-	    Thread[] threads = new Thread[numThreads*2];
-	    numThreads = group.enumerate(threads, false);
-
-	    // Enumerate each thread in `group'
-	    for (int i=0; i<numThreads; i++) {
-	        // Get thread
-	        Thread thread = threads[i];
-	        System.out.println(thread.getName());
-	    }
-
-	    // Get thread subgroups of `group'
-	    int numGroups = group.activeGroupCount();
-	    ThreadGroup[] groups = new ThreadGroup[numGroups*2];
-	    numGroups = group.enumerate(groups, false);
-
-	    // Recursively visit each subgroup
-	    for (int i=0; i<numGroups; i++) {
-	        visit(groups[i], level+1);
-	    }
 	}
 
 	private static DataWriter getWriter(int nodeid, ArrayList<DataWriter> datawriters ){
