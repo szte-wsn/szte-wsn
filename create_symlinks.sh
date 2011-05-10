4526c327c268815666b8609d56a478ed5391e521
Andras Biro <bbandi86@gmail.com>
2011-05-10 09:51:20 +0000

    initial commit
    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@2168 a4e16f3a-606c-4714-833f-ed604d7aca08

---
+++ create_symlinks.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+SOURCE=$SZTETOSROOT
+DEST=$TOSROOT
+DIRS="/tos/chips/
+/tos/platforms/
+/support/make/
+/support/make/avr/
+"
+for d in $DIRS
+do
+	for f in `ls $SZTETOSROOT$d`
+	do
+ 		if [ -e "$TOSROOT$d$f" ]
+ 		then
+ 			echo "$TOSROOT$d$f exists!"
+ 		else
+ 			echo "$TOSROOT$d$f created."
+ 			ln -s $SZTETOSROOT$d$f $TOSROOT$d$f
+ 		fi
+	done
+done
