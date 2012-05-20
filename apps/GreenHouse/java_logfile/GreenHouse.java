9d3fbd37138d3da561e4352013be9ff467167986
Kornel Kalma <Kalman.Kornel.David@stud.u-szeged.hu>
2012-05-20 15:04:33 +0000

    git-svn-id: svn://svn.code.sf.net/p/szte-wsn/code/tinyos-2.x/trunk@3030 a4e16f3a-606c-4714-833f-ed604d7aca08

--- apps/GreenHouse/java_logfile/GreenHouse.java
+++ apps/GreenHouse/java_logfile/GreenHouse.java
@@ -1,33 +1,34 @@
-
-import net.tinyos.message.*;
-import net.tinyos.util.*;
 import java.io.*;
 import java.lang.*;
+import net.tinyos.packet.*;
+import net.tinyos.message.*;
+import net.tinyos.util.*;
 
 
 public class GreenHouse implements MessageListener
 {
-    MoteIF mif;
+    private MoteIF mif;
 	
 	protected java.text.SimpleDateFormat timestamp = new java.text.SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
 
-	public void run(){
-		mif = new MoteIF(PrintStreamMessenger.err);
-		mif.registerListener(new GreenHouseMsg(), this);
+	public GreenHouse(MoteIF mif){
+		this.mif = mif;
+		this.mif.registerListener(new GreenHouseMsg(), this);
 	}
 	
 	public /*synchronized */void messageReceived(int to, Message m){
 		
 		System.out.println("Message received.");
+		System.out.println("Message's dataLength = " + m.dataLength());
+		System.out.println("Message's baseOffset = " + m.baseOffset());
+		
 		if(m instanceof GreenHouseMsg)
 		{
-			GreenHouseMsg ghmsg = (GreenHouseMsg)(m);
+			GreenHouseMsg ghmsg = (GreenHouseMsg) m;
+			
 			int nid = ghmsg.get_source();
 			
 			System.out.println("Correct message format. Sent by mote " + nid + ".");
-			
-			System.out.println("GHMsg: " + ghmsg);
-			System.out.println("Message: " + m);
 			
 			File logfile;
 			FileOutputStream fos = null;
@@ -36,36 +37,6 @@ public class GreenHouse implements MessageListener
 			final String delimiter = " ";
 			
 			logfile = new File("log/mote_" + Integer.toString(nid) + ".txt");
-			/*
-			if( !logfile.exists() )
-			{
-				try
-				{
-					logfile.createNewFile();
-					FileWriter fw = new FileWriter( logfile );
-					//fw.write("Log file of mote with ID" + Integer.toString(nid) );
-					fw.write("Log file is created." );
-				}
-				catch(IOException e)
-				{
-					System.out.println("Unable to create "+logfile+": "+e.getMessage());
-				}
-			}
-			*/
-
-			//Létrehozunk egy FileWriter objektumot, hogy hozzáfûhessünk (2. paraméter = TRUE) a fájlhoz.
-			/*
-			try
-			{
-				FileWriter fw = new FileWriter( logfile, true );
-				//fw.write( timestamp.format(new java.util.Date()) );
-				fw.write( "2012. 01. 30. 11:30" );
-			}
-			catch(IOException e)
-			{
-				System.out.println("Error writing" + logfile +": "+e.getMessage());
-			}
-			*/
 			if( !logfile.exists() )
 			{
 				try
@@ -90,34 +61,21 @@ public class GreenHouse implements MessageListener
 			{
 				fos = new FileOutputStream( logfile, true );
 				pw = new PrintWriter( fos, true );
-				/*
-				int baseOffset = m.baseOffset();
-				int dataLength = m.dataLength();
-				byte[] rawData = m.dataGet();
-				*/
+
 				pw.println( "--------------------------------------" );
-				pw.println( timestamp.format(new java.util.Date()) );
-				/*
-				pw.print("Raw data: ");
-				for(int i = baseOffset; i < baseOffset + dataLength; i++)
-					pw.print(Byte.toString(rawData[i]) + " ");
-				pw.println();
-				*/
+				pw.println( timestamp.format(new java.util.Date()) );				
 				
 				pw.println( "ID of transmitter: " + Integer.toString(ghmsg.get_source()) );
 				pw.println( "Sequence number: " + Integer.toString(ghmsg.get_seqno()) );
 				pw.println( "Parent: " + Integer.toString(ghmsg.get_parent()) );
 				pw.println( "Metric: " + Integer.toString(ghmsg.get_metric()) );
+				
 				pw.print( "Data: " );
 				int[] data = ghmsg.get_data();
 				String dataString = "";
 				for(int i = 0; i < data.length; i++)
-				{
 					dataString += Double.toString( data[i] ) + delimiter;
-				}
 				pw.println(dataString);
-				
-				//pw.println( ghmsg.toString() );
 				
 				pw.close();
 			}
@@ -129,7 +87,23 @@ public class GreenHouse implements MessageListener
 	}
 	
     public static void main(String[] args) {
-		GreenHouse gh = new GreenHouse();
-		gh.run();
+		PhoenixSource phoenix = null;
+		MoteIF mif = null;
+		
+		if( args.length == 0 )
+		{
+			phoenix = BuildSource.makePhoenix( PrintStreamMessenger.err );
+		}
+		else if( args.length == 2 && args[0].equals("-comm") )
+		{
+			phoenix = BuildSource.makePhoenix( args[1], PrintStreamMessenger.err );
+		}
+		else
+		{
+			System.err.println("Usage: java GreenHouse [-comm <source>]");
+			java.lang.System.exit(1);
+		}
+		mif = new MoteIF(phoenix);
+		GreenHouse app = new GreenHouse(mif);
     }
 }
